86d10f675ee8996dab7a9a8c9489e6f3f58704c2
Peter <petergorman11@gmail.com>
2019-04-24 20:09:32 -0400

    Add LoadMultiLineData skeleton function

---
+++ Console Test Enviroment/FileHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Console_Test_Enviroment
+{
+    class FileHandler
+    {
+        public static Dictionary<int, string[]> LoadMultiLineData(string filePath, MultiLineDataOptions options)
+        {
+            if (!File.Exists(filePath))
+                return null;
+
+            Dictionary<int, string[]> fileData = new Dictionary<int, string[]>();
+
+
+
+            return fileData;
+        }
+    }
+
+}
