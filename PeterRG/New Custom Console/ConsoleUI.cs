3fafbbaaf444a305bcde6d9a10ece1f9256bd828
Peter <petergorman11@gmail.com>
2019-05-02 19:52:33 -0400

    Create ConsoleUI.cs

---
+++ PeterRG/New Custom Console/ConsoleUI.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * 
+ * Created by Peter Gorman
+ * Console.cs
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Linq;
+
+namespace PeterRG.CustomConsole
+{
+    class ConsoleUI
+    {
+    }
+}
