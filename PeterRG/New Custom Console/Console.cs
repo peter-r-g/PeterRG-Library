b8a44d4783bb1e71da8ccc3924d0bf8280b47d22
Peter <petergorman11@gmail.com>
2019-05-02 19:52:29 -0400

    Create Console.cs

---
+++ PeterRG/New Custom Console/Console.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * 
+ * Created by Peter Gorman
+ * PeterRGConsole.cs
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Linq;
+
+namespace PeterRG.PeterRGConsole
+{
+    class Console
+    {
+        public static Console instance { get; private set; }
+        private static ConsoleUI consoleInterface;
+
+        public Console()
+        {
+
+        }
+
+    }
+}
